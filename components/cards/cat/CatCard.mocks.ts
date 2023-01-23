ccd9c758138c3f7669f2a3d05b6e84fe2fb1650a
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-01-23 14:42:32 -0600

    feat: implement CatCard component and story

--- components/cards/cat/CatCard.mocks.ts
+++ components/cards/cat/CatCard.mocks.ts
@@ -1,14 +1,13 @@
 import { ICatCard } from './CatCard';
 
 const base: ICatCard = {
-  sampleTextProp: 'Hello World!',
-};
-
-const alt: ICatCard = {
-  sampleTextProp: 'Hello alt World!',
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
 };
 
 export const mockCatCardProps = {
   base,
-  alt,
 };
--- components/cards/cat/CatCard.stories.tsx
+++ components/cards/cat/CatCard.stories.tsx
@@ -5,17 +5,17 @@ import { mockCatCardProps } from './CatCard.mocks';
 export default {
   title: 'cards/CatCard',
   component: CatCard,
-  // parameters: {
-  //   // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-  //   layout: 'fullscreen',
-  // },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
 } as ComponentMeta<typeof CatCard>;
 
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof CatCard> = (args) => (
   <CatCard {...args} />
 );
 
 export const Base = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
 
 Base.args = {
   ...mockCatCardProps.base,
